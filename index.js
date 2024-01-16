fa6a726ef1cb54f5b2e8e5fc1ec63e9551622569
Nathan Cochran <58408117+nacochran@users.noreply.github.com>
2024-01-15 23:14:29 -0600

    Added Comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs"; // can use bcrypt alternatively
 import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
